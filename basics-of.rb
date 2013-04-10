88b8bdea3cebf4cc1e0a758b376e0d1531d5079e
britford <britford@britford.dlink.com>
2013-04-10 18:40:31 +0100

    initial

---
+++ basics-of.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby -w
+# basics-of.rb
+
+/* 		To run a Ruby script, type ruby at a Shell prompt and enter some ruby commands, then enter ^D (i.e. the end of file character) to execute.
+*		
+*		irb is 'interactive ruby': it's a Ruby Shell complete with command-line history, job control etc.
+*/
+
+
+puts 'Ruby is an OO language, though like most it can be used in a procedural, rather non-OO way.'
+puts 'Once a type has been defined, an object instance of that can be created with the standard method \'new\'.'
+
+/* 		Many data types in Ruby have built in methods - like number.abs or 'someText'.length.  This hints that Ruby has fewer 'primitive' data types and represents most data types with some object wrapper.
+*
+
+
