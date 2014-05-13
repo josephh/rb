a9e53016db62a682d25da0e7997364f01ff1eae5
britford <britford@joe.dlink.com>
2014-05-13 22:56:53 +0100

    new html page for objects

--- amazon-book-search.rb
+++ amazon-book-search.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/ruby -w
 # amazon-book-search.rb
-require 'aws/s3'
+require 'amazon/aws'
 
 if ARGV.size !=3
   puts "Usage: #{$0} [Amazon Web Services AccessKey ID] [secret Amazon key] [text to search for]"
   exit
 end
 access_key, secret_key, search_request = ARGV
-conn = AWS::S3::Base.establish_connection!(
-    :access_key_id     => access_key,
-    :secret_access_key => secret_key)
+req = Amazon::AWS::Search:Request.new( :key_id => access_key)
+puts "What does the request look like ? : " + req
+is = Amazon::AWS::ItemSearch.new( 'Books', { 'Title' => 'a tale for the time being' } )
 # for every book...
-conn.keyword_search(search_request, 'books', Amazon::Search::LIGHT) do |book|
+req.search(is) do |book|
   # print book name and list of authors
   puts %{"#{book.product_name}" by #{books.authors.join(',')}}
 end
