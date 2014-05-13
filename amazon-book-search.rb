db40dbf5650a8fb311d236fa420598d61ece3a56
britford <britford@joe.dlink.com>
2014-05-13 23:15:31 +0100

    book search stuff

--- amazon-book-search.rb
+++ amazon-book-search.rb
@@ -1,15 +1,10 @@
 #!/usr/bin/ruby -w
 # amazon-book-search.rb
-require 'amazon/aws'
+require 'amazon/aws/search/request'
 
-if ARGV.size !=3
-  puts "Usage: #{$0} [Amazon Web Services AccessKey ID] [secret Amazon key] [text to search for]"
-  exit
-end
-access_key, secret_key, search_request = ARGV
-req = Amazon::AWS::Search:Request.new( :key_id => access_key)
+req = Request.new('AKIAJUO56EUJUC6HDDRA')
 puts "What does the request look like ? : " + req
-is = Amazon::AWS::ItemSearch.new( 'Books', { 'Title' => 'a tale for the time being' } )
+is = ItemSearch.new( 'Books', { 'Title' => 'a tale for the time being' } )
 # for every book...
 req.search(is) do |book|
   # print book name and list of authors
