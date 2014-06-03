1f3510222406fb3b93e4b78858b7e5db0969189b
britford <britford@joe.dlink.com>
2014-06-03 23:14:54 +0100

    fixed ItemSearch (ruby  uninitialized constant  (NameError)) by explictly including the full path and require statement

--- amazon-book-search.rb
+++ amazon-book-search.rb
@@ -4,8 +4,8 @@ require 'amazon/aws/search'
 require 'amazon/aws'
 
 req = Amazon::AWS::Search::Request.new('AKIAJUO56EUJUC6HDDRA')
-puts "What does the request look like ? : " + req
-is = ItemSearch.new( 'Books', { 'Title' => 'a tale for the time being' } )
+# puts "What does the request look like ? : " + req
+is = Amazon::AWS::ItemSearch.new( 'Books', { 'Title' => 'a tale for the time being' } )
 # for every book...
 req.search(is) do |book|
   # print book name and list of authors
