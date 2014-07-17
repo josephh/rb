f80788d59ec6f5fb5a0eb33cfe0b7354e5618907
britford <josephh@rocketmail.com>
2014-07-17 20:11:12 +0100

    notes

--- amazon-book-search.rb
+++ amazon-book-search.rb
@@ -8,13 +8,14 @@ include Amazon::AWS
 
 # the amazon/aws/search new constructor signature is,  new(key_id=nil, associate=nil, locale=nil, cache=nil, user_agent=USER_AGENT) 
 req = Request.new('AKIAJEJRITLFTFRSEK3A')
-# puts "What does the request look like ? : " + req
+puts "What does the request object look like ? : " + req.inspect()
 is = ItemSearch.new( 'Books', { 'Title' => 'a tale for the time being' } )
 # for every book...
-obj = req.search(is)
-puts("type of aws object returned by the search ? " + obj.inspect())
-puts("number of items found: " + obj.length())
-# -- comment req.search(is) do |book|
+awsObj = req.search(is)
+arr = awsObj.properties()
+arr.each{|prop| puts "next prop " + prop}
+# puts("type of aws object returned by the search ? " + obj.inspect())
+
   # print book name and list of authors
   # -- comment -- puts %{"#{book.product_name}" by #{books.authors.join(',')}}
 # -- comment -- end
