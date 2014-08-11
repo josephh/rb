5fa6512555b2732a174b98e133ba96f8b88b2b72
josephh <josephh@rocketmail.com>
2014-08-11 23:10:36 +0100

    Ian McDs examples...

--- amazon-book-search.rb
+++ amazon-book-search.rb
@@ -23,18 +23,52 @@ is.response_group = rg
 
 # There are different ways to use the aws object returned from the search
 # 1. Pass the result into a block
+nr_items = 0
+page_nr = 0 
 response = req.search(is, :ALL_PAGES) 
+puts "The response object is an array of " + response.length.to_s + " " + response[0].class.to_s + " elements"
   # TODO - figure out how to get to each search response element item (i.e. a page at a time)
-  response[0].item_search_response[0].items.each do |item|
-    puts "NEXT ITEM : " + item.to_s
+  response.each do |page|
+    puts "NEXT PAGE >>>"
+    printf( "Page %d had unique request ID %s.\n", page_nr += 1, page.item_search_response[0].operation_request[0].request_id )
+    printf( "Page %d contained %d result(s).\n", page_nr, page.item_search_response[0].items[0].item.size ) 
+    is_resp = page.item_search_response
+    # Here's a quote from Ian McDonald, Everything we get back from AWS is transformed into an array.
+    puts "The is_resp variable is of type " + is_resp.class.to_s + " (it contains " +is_resp.items.length.to_s + " element(s) of type " + is_resp.items.class.to_s  + ")"
+    is_resp.items.each do |is|
+      puts "NEXT is  >>>"
+        is.each do |e|
+            puts "NEXT e >>>"
+            puts e.class.to_s
+        end 
+        puts "GET SOME DETAILS ABOUT THE ITEM FIELD >>> " 
+        ii = is.item to_s + " of length " + ii.length.to_s
+        puts "PROPERTIES >>> "
+        ii.instance_variables.each do |iv|
+          printf("instance variable name = %s\n", iv, ii.instance_variable_get(iv) )
+        end
+    end
   end 
-# req.search(is, :ALL_PAGES) do |page| 
-#  puts ("NEXT PAGE : ") + page.inspect
-#  page.items do |item|
-#    puts "ITEM :" + item.inspect
-#    item_set.item.each do |iv|
-#      printf("%s = %s\n", iv, item.instance_variable_get(iv))
-#     end
-#  end
-# end
-
+  
+# 2. You don't have to access the items through a block.
+  nr_items = 0
+  response.each do |page|
+    page.item_search_response[0].items.each do |item_set|
+      nr_items += item_set.item.size
+      printf( "The next item returned %d items.\n", item_set.item.size)
+    end
+  end
+  printf( "Search returned %d items.\n", nr_items )
+  # The first item in the list.
+  items = response[0].item_search_response[0].items[0].item
+  product1 = items[0]
+  puts "\nProperties available for the first product returned:",
+  product1.properties.sort
+  puts
+  
+  # There are three ways to retrieve the property of a product:
+  #
+  # Instance variable:
+  #
+  p product1.asin
+  p product1.item_attributes[0].title 
